9c2f9f180d189a5c5876780e51733ed03cbb841f
Danny Festor <danny@festor.info>
2020-11-12 10:36:42 +0900

    Use Tailwind Form for Checkbox

--- resources/views/components/input-checkbox.blade.php
+++ resources/views/components/input-checkbox.blade.php
@@ -1,4 +1,5 @@
 <div>
     <label for="{{ $name }}">{{ $slot }}</label>
-    <input type="checkbox" name="{{ $name }}" id="{{ $name }}" {{ $checked ? "checked" : "" }}>
+    <input type="checkbox" name="{{ $name }}" id="{{ $name }}" {{ $checked ? "checked" : "" }}
+        class="form-checkbox h-5 w-5 text-blue-400">
 </div>
