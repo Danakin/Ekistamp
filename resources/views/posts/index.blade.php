f635b029125e0013dd5c09cbf5ecf1a692cecca2
Danny Festor <danny@festor.info>
2020-11-12 14:05:12 +0900

    Small Design Changes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,14 @@
 @extends('partials.app')
 
 @section('content')
-@foreach ($posts as $post)
-<article class="shadow-md mb-4 p-4 rounded-md">
-    <a href="{{ route('posts.show', $post) }}">
-        <h2 class="text-xl font-bold text-ekigreen hover:text-green-400">{{ $post->title }}</h2>
-        <p>{{ $post->shortdescription }}</p>
-    </a>
-</article>
-@endforeach
+<section class="divide-green-100 divide-y-2">
+    @foreach ($posts as $post)
+    <article class="mb-4 p-4 transform duration-150 hover:scale-110">
+        <a href="{{ route('posts.show', $post) }}">
+            <h2 class="text-xl font-bold text-ekigreen hover:text-green-400">{{ $post->title }}</h2>
+            <p>{{ $post->shortdescription }}</p>
+        </a>
+    </article>
+    @endforeach
+</section>
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('partials.app')
 
 @section('content')
-<article class="shadow-md mb-4 p-4 rounded-md">
+<article>
     <h2 class="text-xl font-bold">{{ $post->title }}</h2>
     <p>{{ $post->description }}</p>
 </article>
